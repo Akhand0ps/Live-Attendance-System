ff569be73e44f9c61414f1be3d0ef3eda4b2c6a9
amandollar <toxicempire3@gmail.com>
2026-01-10 00:59:50 +0530

    update config and imports

--- src/middleware/auth.role.ts
+++ src/middleware/auth.role.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express-serve-static-core'
+import { Request, Response, NextFunction } from 'express'
 import { success } from 'zod'
 import fa from 'zod/v4/locales/fa.js'
 
